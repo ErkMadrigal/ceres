f83b5f6f67088d78f4da4a6dd850cf0e2695670e
Erick <erick.madfl@outlook.com>
2020-08-26 22:18:27 -0500

    nuevos cmabios

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const app = express();
 //     .catch(err => console.log('type error mongoDB', err));
 
 //settings
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, '/views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
